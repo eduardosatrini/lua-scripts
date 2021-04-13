ce89f089ceb74287da609a59fb41137f4ca6a3d5
Eduardo Henrique <eduardosatrini@gmail.com>
2021-04-13 17:31:34 -0300

    add factorial script

---
+++ factorial.lua
@@ -0,0 +1,13 @@
+-- Factorial recursive
+
+function factorial(num)
+	if num == 0 then
+		return 1
+	else
+		return num * factorial(num - 1)
+	end
+end
+
+print("Enter a number for calc factorial: ")
+num = io.read("*number") --> read a number
+print(factorial(num)) --> show factorial
